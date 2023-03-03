c4f2ab7cb270d7aa0bfca23a4e10a920e8241443
Serj <71671989+serjsankov@users.noreply.github.com>
2023-03-04 00:02:28 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require("sequelize");
 
 module.exports = new Sequelize(
-  "serjsapq_bot",
-  "serjsapq_bot",
+  "serjsapq_new_bot",
+  "serjsapq_new_bot",
   "Serega141297!",
   {
     host: "serjsapq.beget.tech",
